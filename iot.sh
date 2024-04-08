fcaa0918afd99529c757b1fa2e7a57a4df02cde0
pskrsk <166392300+pskrsk@users.noreply.github.com>
2024-04-08 16:51:34 +0530

    Add files via upload

---
+++ iot.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+read -p "please enter first number"x
+echo "first number is $x"
+
+read -p "please enter second number"y
+
+echo "second number is $y"
+let sum = $x+$y
+echo "sum of two number is $sum"
